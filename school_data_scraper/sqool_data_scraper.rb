4b22ea0c05c691d850bda473ebdd671fb7e8a0f5
Matteo Melani <matteomelani@gmail.com>
2011-04-29 11:40:41 -0700

    added geolocation to scrapping so that we do not bother google geocoder

--- school_data_scraper/sqool_data_scraper.rb
+++ school_data_scraper/sqool_data_scraper.rb
@@ -1,23 +1,46 @@
 require 'nokogiri'
 require 'open-uri'
+require 'yaml'
+require 'geokit'
 
+i=0
+school_data_hash ={}
+target_city = [ "menlo park", "palo alto" ]
+target_city.each  do |city|
 
-url="http://www.greatschools.org/search/search.page?c=school&q=94025&state=CA"
-print "\n\n\nScraping #{url} ...\n\n"
-doc = Nokogiri::HTML(open(url))
-school_list = []
-search_results = doc.css('tr.school-search-result-row')
-search_results.each do |sd|
-  school_data ={}
-  school_data[:name] = Nokogiri::HTML(sd.to_s).css('td .js-school-search-result-name')[0].text
-  school_data[:type] = Nokogiri::HTML(sd.to_s).css('td .text3 span')[0].text
-  school_data[:grade] = Nokogiri::HTML(sd.to_s).css('td .text3 span+span')[0].text
-  school_data[:street] = Nokogiri::HTML(sd.to_s).css('td .js-school-search-result-street')[0].text
-  school_data[:citystatezip] = Nokogiri::HTML(sd.to_s).css('td .js-school-search-result-citystatezip')[0].text
-  school_data[:phone] = Nokogiri::HTML(sd.to_s).css('td .js-school-search-result-phone')[0].text
-  print school_data
-  print "\n\n"
+  print "scraping school data for: #{city}.\n"
+  url = sprintf("http://www.greatschools.org/search/search.page?c=school&q=%s&state=CA&pageSize=100", city.strip.upcase.sub(' ', "+"))
+  print "scraping #{url} ..."
+  doc = Nokogiri::HTML(open(url))
+  search_results = doc.css('tr.school-search-result-row')
+  search_results.each do |sd|
+    school_data ={}
+    school_data[:name] = Nokogiri::HTML(sd.to_s).css('td .js-school-search-result-name')[0].text.strip
+    school_data[:category] = Nokogiri::HTML(sd.to_s).css('td .text3 span')[0].text.downcase.strip
+    school_data[:grade_range] = Nokogiri::HTML(sd.to_s).css('td .text3 span+span')[0].text.downcase.strip
+    citystatezip = (Nokogiri::HTML(sd.to_s).css('td .js-school-search-result-citystatezip')[0].text).split(',')
+    statezip = citystatezip[1].split(' ')
+
+    address = {
+      :street  => Nokogiri::HTML(sd.to_s).css('td .js-school-search-result-street')[0].text,
+      :city    => citystatezip[0].strip,
+      :state   => statezip[0].strip,
+      :zipcode => statezip[1].strip.to_i
+      }
+    address[:geolocation] = Geokit::Geocoders::GoogleGeocoder.geocode("#{address[:street]}, #{address[:city]}, #{address[:state]}")
+    school_data = school_data.merge :address=>address, :phone_number=>"" 
+    school_data_hash[i] = school_data
+    i += 1
+  end
+  puts "done.\n"
+  print "scrapted data for #{search_results.count} #{city} schools.\n"   
 end
-print search_results.count
-print "\n\nDone!\n\n"
 
+print "gather data for #{school_data_hash.size} school in total.\n"
+output_file = "school_data.yaml"
+f = File.open(output_file, "w")
+print "writing data to file: #{File.absolute_path(f)} ..." 
+f.puts school_data_hash.to_yaml
+f.close
+puts "done.\n"
+print "good-bye.\n\n"
